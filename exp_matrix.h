dc69f5450b043b63a61bdb2277fa63162922a3ac
Michael Wolfinger <mtw@tbi.univie.ac.at>
2003-09-10 13:56:38 +0000

    initial revision

---
+++ exp_matrix.h
@@ -0,0 +1,5 @@
+/* exp_matrix.h */
+/* Last changed Time-stamp: <2003-08-27 19:37:09 mtw> */
+/* static char rcsid[] = "$Id: exp_matrix.h,v 1.1 2003/09/10 13:56:38 mtw Exp $"; */
+
+void padexp(double *from,double *out,int n,int ord);
