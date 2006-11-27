2c96679c6357d9e6bddda3436962f5efdecad1f7
Michael Wolfinger <mtw@tbi.univie.ac.at>
2006-11-27 13:47:57 +0000

    removed reading of binray rates file\nincluded MxRecover functionality

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 /*=   main.c                                                      =*/
 /*=   main file for treekin                                       =*/
 /*=   ---------------------------------------------------------   =*/
-/*=   Last changed Time-stamp: <2006-11-24 17:49:23 mtw>          =*/
-/*=   $Id: main.c,v 1.23 2006/11/24 16:50:15 mtw Exp $            =*/
+/*=   Last changed Time-stamp: <2006-11-27 13:09:09 mtw>          =*/
+/*=   $Id: main.c,v 1.24 2006/11/27 13:47:57 mtw Exp $            =*/
 /*=   ---------------------------------------------------------   =*/
 /*=                 (c) Michael Thomas Wolfinger                  =*/
 /*=                      mtw@tbi.univie.ac.at                     =*/
@@ -27,19 +27,15 @@ main (int argc, char **argv)
   BarData *Data=NULL;
   double *U=NULL, *S=NULL, *p0=NULL, *p8=NULL, *R=NULL;
   int  dim;
-
+ 
   parse_commandline(argc, argv);
  
   if(opt.method == 'F')      /* full process */
     dim = ParseInfile(opt.INFILE, &R);
   else
     dim = ParseBarfile (opt.INFILE, &Data);
-  if(opt.method == 'I'){
-    if (opt.binrates)
-      dim =  MxBinRead(&R);
-    else
-      ParseRatesFile(&R, dim);
-  }
+  if(opt.method == 'I')
+    ParseRatesFile(&R, dim);
   MxInit (dim);
   U  = MxBar2Matrix(Data, R);
   MxGetSpace(&p8);
@@ -58,7 +54,10 @@ main (int argc, char **argv)
 
   if(opt.matexp) MxExponent(p0,p8,U);
   else{
-    MxDiagonalize (U, &S, p8);
+    if(opt.rrecover) /* read pre-calculated eigenv{alues,ectors} */
+      MxRecover(&S, p8);
+    else
+      MxDiagonalize (U, &S, p8);
     MxIterate (p0, p8, S);
     free(S);free(p8);
   }
